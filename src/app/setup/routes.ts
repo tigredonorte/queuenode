ae219b8e1abf9ff2cb4316b8cd4c213d5343aa98
Thompson <thomfilg@gmail.com>
2024-10-05 08:24:59 -0300

    refactor admin routes

--- src/app/setup/routes.ts
+++ src/app/setup/routes.ts
@@ -1,17 +1,16 @@
 import { Express } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from '../../../build/swagger.json';
-import { createQueueController } from '../controllers/admin/QueuesController';
+import { adminRouter } from '../controllers/admin/admin.routes';
 import { userRouter } from '../controllers/user/user.routes';
 
 export const setupRoutes = (app: Express) => {
-  const serverAdapter = createQueueController('/admin/queues');
   app.get('/', (req, res) => res.status(200).send('Hello World'));
-  app.use('/admin/queues', serverAdapter.getRouter());
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.get("/debug-sentry", function mainHandler(req, res) {
     throw new Error("My first Sentry error!");
   });
 
+  app.use('/admin', adminRouter);
   app.use('/user', userRouter);
 };
